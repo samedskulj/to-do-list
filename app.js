8809183c238b899e08be9d75797963a6599f1fca
Samed Skulj <63544749+samedskulj@users.noreply.github.com>
2020-10-25 14:04:28 +0100

    Added files

---
+++ app.js
@@ -0,0 +1,35 @@
+const listaStvari = document.querySelector(".listeItema");
+const vrijednost = document.querySelector("#todolist");
+let dugmeMalo = document.querySelector(".dugme");
+let brisanjeListe = document.querySelector(".brisanje");
+const ispod = document.querySelector(".ispod");
+
+
+dugmeMalo.addEventListener("click", ()=>{
+    if (vrijednost.value == "") {
+        ispod.style.visibility = "visible";
+    } else if(vrijednost.value != "") {
+        ispod.style.visibility = "hidden";
+        var lista = document.createElement("li");
+        var dugmeZaListe = document.createElement("button");
+        dugmeZaListe.classList.add("dugmeListeStyle")
+        dugmeZaListe.innerText = "Izbrisi";
+        lista.classList.add("styleListe");
+        let text = document.createTextNode(vrijednost.value);
+        lista.appendChild(text);
+        lista.appendChild(dugmeZaListe);
+        listaStvari.appendChild(lista);
+
+        vrijednost.value = "";
+        dugmeZaListe.addEventListener("click", ()=> {
+            listaStvari.removeChild(lista);
+        })
+         brisanjeListe.addEventListener("click", ()=> {
+             listaStvari.removeChild(lista);
+         })
+    }   
+})
+
+
+
+
